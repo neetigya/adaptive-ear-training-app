2b6df69e501dd692c89bd235b519b969ee12a9cc
Neetigya Saxena <neetigya@gmail.com>
2026-02-16 14:20:38 -0600

    Update package dependencies and add new training routes. Added @types/react and @types/react-dom to devDependencies. Introduced new routes for interval, chord, scale, and triads in the training section.

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,19 @@ Route::get('/', function () {
 Route::get('/training/note', function () {
     return Inertia::render('Training/Note');
 });
+
+Route::get('/training/interval', function () {
+    return Inertia::render('Training/Interval');
+});
+
+Route::get('/training/chord', function () {
+    return Inertia::render('Training/Chord');
+});
+
+Route::get('/training/scale', function () {
+    return Inertia::render('Training/Scale');
+});
+
+Route::get('/training/triads', function () {
+    return Inertia::render('Training/Triads');
+});
